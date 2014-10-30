1e3131408ed8d023c7d0902495a110226b40a4d5
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-30 16:16:20 +0000

    working recursive inject/reduce

--- lib/reduce.rb
+++ lib/reduce.rb
@@ -1,8 +1,6 @@
 class Array
   def my_inject (sum, &block)
-    # change order!
-    return sum if self.empty?
-    #self.drop(1).my_inject(sum,&block) + block.call(sum, self.first) 
+    return sum if self.empty? 
     self.drop(1).my_inject(block.call(sum, self.first),&block) 
   end
 end
